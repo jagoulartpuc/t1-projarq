675ddf87ecfd230e6076a7b5882685b3a0d69692
12105790 <12105790@W1846922.portoalegre.pucrsnet.br>
2019-09-18 20:16:10 -0300

    SpringBoot config

---
+++ hackatona-app/src/main/java/br/pucrs/projArqTrab1/hackatona/hackatonaapp/HackatonaAppApplication.java
@@ -0,0 +1,13 @@
+package br.pucrs.projArqTrab1.hackatona.hackatonaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonaAppApplication.class, args);
+	}
+
+}
