11104220c078658f0e8539526ca7ee0408a9e035
Maurício de Carvalho Lima <mauricio.lima@edu.pucrs.br>
2019-09-22 21:00:16 -0300

    IndexController views html

--- src/main/java/pucrs/projarq/t1/T1Application.java
+++ src/main/java/pucrs/projarq/t1/T1Application.java
@@ -2,7 +2,10 @@ package pucrs.projarq.t1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
+@ComponentScan({"pucrs.projarq.t1.controller","pucrs.projarq.t1.domain","pucrs.projarq.t1.repository","pucrs.projarq.t1.service"})
 @SpringBootApplication
 public class T1Application {
 
